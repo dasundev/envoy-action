354e5ea81fafa04ad7213f030c5a87537b351fd8
Dasun Tharanga <hello@dasun.dev>
2024-05-17 14:32:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { exec } from '@actions/exec'
 import * as core from '@actions/core'
 
 try {
-    const nameToGreet = core.getInput('who-to-greet');
+    const nameToGreet = core.getInput('story');
     console.log(`Hello ${nameToGreet}!`);
 } catch (error) {
     core.setFailed(error.message);
