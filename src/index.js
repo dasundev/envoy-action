97af3a67ab419278eaf72b8038649eb5e2c7c868
Dasun Tharanga <hello@dasun.dev>
2024-05-17 17:09:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import * as core from '@actions/core'
 try {
     const story = core.getInput('story');
 
-    // Install Laravel Envoy
-    await exec('composer require laravel/envoy --dev')
-
     // Deploy
     await exec('./vendor/bin/envoy run ' + story)
 } catch (error) {
