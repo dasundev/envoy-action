726c33e154486dcd3a55479ad0c27a9c7b9dbf44
Dasun Tharanga <hello@dasun.dev>
2024-05-17 15:42:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { exec } from '@actions/exec'
 import * as core from '@actions/core'
+import * as utils from './utils.js'
 
-try {
-    const story = core.getInput('story');
+await utils.getServers()
 
-    // Install Laravel Envoy
-    await exec('composer require laravel/envoy --dev')
+// try {
+//     const story = core.getInput('story');
 
-    // Deploy
-    await exec('./vendor/bin/envoy run ' + story)
-} catch (error) {
-    core.setFailed(error.message);
-}
+//     // Install Laravel Envoy
+//     await exec('composer require laravel/envoy --dev')
+
+//     // Deploy
+//     await exec('./vendor/bin/envoy run ' + story)
+// } catch (error) {
+//     core.setFailed(error.message);
+// }
